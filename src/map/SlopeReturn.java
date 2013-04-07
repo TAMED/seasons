2726867566818fc7026b48f652b339f3bff2c77d
Mike Salvato <msalvato@dendrite.mit.edu>
2013-04-07 00:19:47 -0400

    whoops added files now

---
+++ src/map/SlopeReturn.java
@@ -0,0 +1,15 @@
+package map;
+
+import util.Corner;
+
+public class SlopeReturn {
+	public int i;
+	public int j;
+	public Corner corner;
+	
+	SlopeReturn(int i, int j, Corner corner) {
+		this.i = i;
+		this.j = j;
+		this.corner = corner;
+	}
+}
