d795032a3cabda68679344527c52e94111ea6b35
MikeSalvato@gmail.com <msalvato@dendrite.mit.edu>
2013-04-21 21:52:19 -0400

    new map tiles. Woo

---
+++ assets/maps/wtf.tsx
@@ -0,0 +1,90 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="forestTiles" tilewidth="32" tileheight="32">
+ <image source="forestTiles.png" width="384" height="384"/>
+ <tile id="7">
+  <properties>
+   <property name="endpoint" value="first"/>
+   <property name="flipped" value="false"/>
+   <property name="slope" value="1"/>
+   <property name="type" value="slope"/>
+  </properties>
+ </tile>
+ <tile id="8">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="9">
+  <properties>
+   <property name="endpoint" value="first"/>
+   <property name="flipped" value="false"/>
+   <property name="slope" value="-1"/>
+   <property name="type" value="slope"/>
+  </properties>
+ </tile>
+ <tile id="12">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="13">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="14">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="18">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="19">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="23">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="24">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="25">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="26">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="type" value="wall"/>
+  </properties>
+ </tile>
+</tileset>
