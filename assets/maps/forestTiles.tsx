0d09a6c053eb75f5fd3861bec174b4ad27f614d1
MikeSalvato@gmail.com <msalvato@dendrite.home>
2013-04-22 04:04:33 -0400

    fixed slope is tilemap

--- assets/maps/forestTiles.tsx
+++ assets/maps/forestTiles.tsx
@@ -323,7 +323,7 @@
  </tile>
  <tile id="99">
   <properties>
-   <property name="endpoint" value="first"/>
+   <property name="endpoint" value="second"/>
    <property name="flipped" value="false"/>
    <property name="slope" value="2"/>
    <property name="type" value="slope"/>
@@ -331,7 +331,7 @@
  </tile>
  <tile id="100">
   <properties>
-   <property name="endpoint" value="second"/>
+   <property name="endpoint" value="first"/>
    <property name="flipped" value="false"/>
    <property name="slope" value="2"/>
    <property name="type" value="slope"/>
@@ -347,7 +347,7 @@
  </tile>
  <tile id="110">
   <properties>
-   <property name="endpoint" value="second"/>
+   <property name="endpoint" value="first"/>
    <property name="flipped" value="false"/>
    <property name="slope" value="2"/>
    <property name="type" value="slope"/>
