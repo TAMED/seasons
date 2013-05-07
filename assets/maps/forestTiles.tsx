f4ac642a4c24fef6db7eb6a3b93f3d76a2de3e5d
dheins <daheins@gmail.com>
2013-05-06 21:46:23 -0400

    Changed the doors in all our new levels. Also my bear roar is in now...

--- assets/maps/forestTiles.tsx
+++ assets/maps/forestTiles.tsx
@@ -1,11 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset name="forestTiles" tilewidth="32" tileheight="32">
  <image source="forestTiles.png" width="384" height="384"/>
- <tile id="0">
-  <properties>
-   <property name="type" value="player"/>
-  </properties>
- </tile>
  <tile id="7">
   <properties>
    <property name="endpoint" value="first"/>
@@ -443,4 +438,9 @@
    <property name="water" value="true"/>
   </properties>
  </tile>
+ <tile id="93">
+  <properties>
+   <property name="type" value="player"/>
+  </properties>
+ </tile> 
 </tileset>
